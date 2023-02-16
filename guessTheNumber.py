ec2cc8f44b4c2bf6a3b099174f3b9b53a1aa9afd
José Freites <jlfu2004@gmail.com>
2023-02-16 12:19:19 -0300

    ADD: coments, / FIXED: logical errors

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,1 +1,34 @@
 import random
+
+tryDidIt = 0
+
+print('¡Hola! ¿Cómo te llamas?')
+myName = input()
+
+number = random.randint(1, 20)
+print('Bueno, ' + myName + ', estoy pensando en un número entre 1 y 20.')
+
+while tryDidIt < 6:
+    print('Intenta adivinar.')  # there are four spaces after print
+    guessing = input()
+    guessing = int(guessing)
+
+    tryDidIt = tryDidIt + 1
+
+    if guessing < number:
+        print('Tu estimación es muy baja.')  # we have eight spaces after print
+
+    if guessing > number:
+        print('Tu estimación es muy alta.')
+
+    if guessing == number:
+        break
+
+if guessing == number:
+    tryDidIt = str(tryDidIt)
+    print('¡Buen trabajo, ' + myName +
+          '! ¡Has adivinado mi número en ' + tryDidIt + ' intentos!')
+
+if guessing != number:
+    number = str(number)
+    print('Pues no. El número que estaba pensando era ' + number)
